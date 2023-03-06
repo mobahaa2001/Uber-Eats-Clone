674cb98d1c6efb5b64b4881ab68e88fe78e75fe0
Mego <70282836+mobahaa2001@users.noreply.github.com>
2023-03-06 13:49:51 +0200

    Update HeaderTaps.tsx

--- src/Components/Header/HeaderTaps.tsx
+++ src/Components/Header/HeaderTaps.tsx
@@ -1,24 +1,40 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function HeaderTaps() {
-  return (
-    <View style={{flexDirection: "row", alignSelf:"center"}}>
-        <HeaderButton text="Delivery" btnColor="black" textColor = "white"/>
-        <HeaderButton text="Pickup" btnColor="white" textColor = "black"/>
-    </View>
-  )
+    const [btnTap,setBtnTap] = useState("Delivery")
+    return (
+      <View style={{flexDirection: "row", alignSelf:"center"}}>
+        <HeaderButton 
+            text="Delivery"
+            btnColor="black"
+            textColor = "white"
+            btnTap={btnTap}
+            setBtnTap={setBtnTap}
+        />
+        <HeaderButton 
+            text="Pickup" 
+            btnColor="white" 
+            textColor = "black"
+            btnTap={btnTap}
+            setBtnTap={setBtnTap}
+        />
+      </View>
+    )
 }
 
 const HeaderButton = (props) => (
-    <TouchableOpacity style={{
-        backgroundColor: props.btnColor,
-        paddingVertical: 6,
-        paddingHorizontal: 16,
-        borderRadius: 30
-    }}>
+    <TouchableOpacity 
+        style={{
+            backgroundColor: props.btnTap == props.text?"black": "white",
+            paddingVertical: 6,
+            paddingHorizontal: 16,
+            borderRadius: 30
+        }}
+        onPress={() => props.setBtnTap(props.text)}
+    >
         <Text style={{
-            color:props.textColor,
+            color:props.btnTap == props.text? "white" : "black",
             fontSize:15,
             fontWeight: "900"   
         }}>{props.text}</Text>
